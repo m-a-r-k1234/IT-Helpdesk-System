ed371fc427024a87ea666ce3e279893a6c398af3
Mark Jedediah Padilla <padillamj80@gmail.com>
2026-09-21 13:34:39 +0800

    just changes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from app.database import Base, engine
 from app.models import User, Category, Ticket, Comment, TicketHistory
 from app.routers.auth import router as auth_router
-
+from app.core.dependencies import get_current_user
+from app.models.user import User
 
 Base.metadata.create_all(bind=engine)
 
@@ -22,4 +23,16 @@ app.include_router(auth_router)
 def root():
     return {
         "message": "IT Help Desk API is running"
+    }
+
+@app.get("/me")
+def get_me(
+    current_user: User = Depends(get_current_user)
+):
+    return {
+        "id": current_user.id,
+        "first_name": current_user.first_name,
+        "last_name": current_user.last_name,
+        "email": current_user.email,
+        "role": current_user.role
     }
